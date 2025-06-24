f7998dd80be22622513a48c5c3a5093d0eb9f216
bhanu256-code <bhanugorle12@gmail.com>
2025-06-24 23:18:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+  "name": "telegram-dialogflow-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2",
+    "node-telegram-bot-api": "^0.61.0",
+    "@google-cloud/dialogflow": "^4.3.2",
+    "uuid": "^9.0.0"
+  }
+}
